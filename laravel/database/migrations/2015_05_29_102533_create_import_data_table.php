17ddce1a0b9cd42cf0906088383d299f56695ff1
Kimbsy <dave.kimber@simitive.com>
2015-05-29 11:33:08 +0100

    set up some migrations

---
+++ laravel/database/migrations/2015_05_29_102533_create_import_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImportDataTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('import_data', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('import_id');
+			$table->longText('data');
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('import_data');
+	}
+
+}
